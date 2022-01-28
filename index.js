f8a0652ad6d99ed6848d56071803ecd4d91b8987
SaiHarsha99 <busettiharsha2197@gmail.com>
2022-01-28 21:32:31 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-import React, { memo, useCallback, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
-import Timer from './timer'
+import Timer from './timer';
 
 const App = () => {
-  const [start, setstart] = useState(true);
-  const [pause, setpause] = useState(false);
-  const [seconds, setseconds] = useState(0);
+  const [active, setactive] = useState(true);
+  const [time, settime] = useState(0);
   const [storeinterval, setstoreinterval] = useState(null);
 
-  const handleStart = (e) => {
+  const handleactive = (e) => {
     useInterval();
-    setstart(false);
-    setpause(true);
-  };
-
-  const handlePause = (e) => {
-    useInterval();
-    setstart(true);
-    setpause(false);
+    setactive(!active);
   };
 
   const useInterval = () => {
-    if (!start) {
+    if (!active) {
       clearInterval(storeinterval);
     } else {
       let interval = setInterval(() => {
-        setseconds((sec) => sec + 10);
+        settime((sec) => sec + 10);
       }, 10);
       setstoreinterval(interval);
     }
   };
 
   return (
-    <div className='stop-watch'>
-      <Timer time={seconds}/>
+    <div className="stop-watch">
+      <Timer time={time} />
       <div className="Control-Buttons">
-        <div ></div>
-      {start && <button className="btn btn-one" onClick={handleStart}>Start</button>}
-      {pause && <button className="btn btn-one" onClick={handlePause}>Pause</button>}
-      <button className="btn btn-two" onClick={() => setseconds(0)}>Reset</button>
+        <button className="btn btn-one" onClick={handleactive}>
+          {' '}
+          {active ? 'Start' : 'Pause'}
+        </button>
+        <button className="btn btn-two" onClick={() => settime(0)}>
+          Reset
+        </button>
       </div>
     </div>
   );
